9fa2c06649173020a867fe1e7d20254994ec6cf4
Admin <erlan4ikaa@gmail.com>
2022-09-07 19:57:34 +0300

    birinchi commit

--- src/com/peaksoft/Main.java
+++ src/com/peaksoft/Main.java
@@ -3,5 +3,9 @@ package com.peaksoft;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Салам алейкум hello");
+
+        Student student = new Student("Izat", 29);
+
+        System.out.println(student);
     }
 }
